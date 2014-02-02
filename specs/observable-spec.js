8dedbee617ed1d0fcd70b53017c470210574c551
Avenger7x <avenger7x13@gmail.com>
2014-02-02 12:40:50 +0200

    split specs

--- specs/observable-spec.js
+++ specs/observable-spec.js
@@ -38,29 +38,3 @@ describe('observable', function(){
 		expect(subscriber.fn).not.toHaveBeenCalled();
 	});
 });
-
-describe('computed', function(){
-	var observable, subscriber, value;
-
-	beforeEach(function(){
-		observable = reactive.observable();
-		subscriber = { fn: function(){
-			return observable();
-		} };
-		value = 13;
-	});
-
-	it('should call fn on create', function(){
-		spyOn(subscriber, 'fn');
-		reactive.computed(subscriber.fn);
-		expect(subscriber.fn).toHaveBeenCalled();
-	});
-
-	it('should compute value when dependencies were changed', function(){
-		var computed = reactive.computed(subscriber.fn);
-
-		observable(value);
-		expect(computed()).toBe(value);
-		expect(computed()).toBe(subscriber.fn());
-	});
-});
