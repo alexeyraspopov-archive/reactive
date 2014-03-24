684e617d63c88fc03b4c3480163e2e581505ef13
Avenger7x <avenger7x13@gmail.com>
2014-03-24 20:02:56 +0200

    specs

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 'use strict';
+
+exports.observable = function(value){
+	var cell, subscribers;
+
+	cell = function(newValue){
+		var changed;
+
+		if(arguments.length){
+			changed = value !== newValue;
+			value = newValue;
+
+			if(changed){
+				cell.notify(value);
+			}
+		}
+
+		return value;
+	};
+
+	subscribers = [];
+
+	cell.subscribe = function(fn){
+		if(subscribers.indexOf(fn) < 0){
+			subscribers.push(fn);
+		}
+
+		return cell;
+	};
+
+	cell.unsubscribe = function(fn){
+		var index = subscribers.indexOf(fn);
+
+		if(index > -1){
+			subscribers.splice(index, 1);
+		}
+
+		return cell;
+	};
+
+	cell.notify = function(data){
+		subscribers.forEach(function(fn){
+			fn(data);
+		});
+
+		return cell;
+	};
+
+	return cell;
+};
--- spec.js
+++ spec.js
@@ -1,1 +1,68 @@
+/* global describe, it */
 'use strict';
+var should, reactive;
+
+should = require('should');
+reactive = require('./index');
+
+describe('observable', function(){
+	it('should be a function', function(){
+		reactive.observable().should.be.a.instanceOf(Function);
+	});
+
+	it('should save inital value', function(){
+		var value, observable;
+
+		value = 13;
+		observable = reactive.observable(value);
+
+		observable().should.be.exactly(value);
+	});
+
+	it('should save value when called with argument', function(){
+		var value, observable;
+
+		value = 13;
+		observable = reactive.observable();
+
+		observable(value);
+		observable().should.be.exactly(value);
+	});
+
+	it('should publish value for subscribers', function(){
+		var value, observable, wasCalled, wasCalledWith;
+
+		value = 13;
+		observable = reactive.observable();
+		wasCalled = false;
+
+		observable.subscribe(function(newValue){
+			wasCalled = true;
+			wasCalledWith = newValue;
+		});
+
+		observable(value);
+
+		wasCalled.should.be.exactly(true);
+		wasCalledWith.should.be.exactly(value);
+	});
+
+	it('should remove subscriber', function(){
+		var value, observable, wasCalled, subscriber;
+
+		value = 13;
+		observable = reactive.observable();
+		wasCalled = false;
+
+		subscriber = function(){
+			wasCalled = true;
+		};
+
+		observable.subscribe(subscriber);
+		observable.unsubscribe(subscriber);
+
+		observable(value);
+
+		wasCalled.should.be.exactly(false);
+	});
+});
