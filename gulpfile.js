efca20956c41cf2af70a356422830baf60a081d4
Avenger7x <avenger7x13@gmail.com>
2014-04-05 19:47:42 +0300

    wrap exports

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	complexity = require('gulp-complexity'),
-	jasmine = require('gulp-jasmine');
+	jasmine = require('gulp-jasmine'),
+	wrap = require('gulp-wrap-exports'),
+	rename = require('gulp-rename');
 
 gulp.task('analysis', function(){
-	return gulp.src('*.js')
+	return gulp.src(['*.js', '!reactive.js'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish'))
 		.pipe(complexity());
@@ -15,6 +17,13 @@ gulp.task('analysis', function(){
 gulp.task('test', function(){
 	return gulp.src('spec.js')
 		.pipe(jasmine());
+});
+
+gulp.task('wrap', ['default'], function(){
+	return gulp.src('index.js')
+		.pipe(wrap({ name: 'reactive' }))
+		.pipe(rename('reactive.js'))
+		.pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['analysis', 'test']);
---
+++ reactive.js
@@ -0,0 +1,106 @@
+!function(exports, global){
+global.reactive = exports;
+'use strict';
+
+var deps, recording;
+
+function record(cell){
+	if(recording && deps.indexOf(cell) < 0){
+		deps.push(cell);
+	}
+}
+
+function startRecord(){
+	deps = [];
+	recording = true;
+}
+
+function getDeps(){
+	recording = false;
+
+	return deps;
+}
+
+exports.observable = function(value){
+	var cell, subscribers;
+
+	cell = function(newValue){
+		var changed;
+
+		if(arguments.length){
+			changed = cell.comparator(value, newValue);
+			value = newValue;
+
+			if(changed){
+				cell.notify(value);
+			}
+		}
+
+		record(cell);
+
+		return value;
+	};
+
+	cell.comparator = function(value, newValue){
+		return value !== newValue;
+	};
+
+	subscribers = [];
+
+	cell.subscribe = function(fn){
+		if(subscribers.indexOf(fn) < 0){
+			subscribers.push(fn);
+		}
+	};
+
+	cell.unsubscribe = function(fn){
+		var index = subscribers.indexOf(fn);
+
+		if(index > -1){
+			subscribers.splice(index, 1);
+		}
+	};
+
+	cell.notify = function(data){
+		subscribers.forEach(function(fn){
+			fn(data);
+		});
+	};
+
+	cell.extend = function(options){
+		Object.keys(options).filter(function(key){
+			return exports.extenders.hasOwnProperty(key);
+		}).forEach(function(key){
+			exports.extenders[key](cell, options[key]);
+		});
+
+		return cell;
+	};
+
+	return cell;
+};
+
+exports.computed = function(read){
+	var data = exports.observable();
+
+	startRecord();
+	data(read());
+
+	getDeps().forEach(function(cell){
+		cell.subscribe(function(){
+			data(read());
+		});
+	});
+
+	return data;
+};
+
+exports.extenders = {
+	notify: function(cell, option){
+		cell.comparator = option === 'always' ? function(){
+			return true;
+		} : cell.comparator;
+	}
+};
+
+}({}, function(){ return this; }());
